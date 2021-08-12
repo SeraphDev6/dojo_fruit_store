2038e4250a104c0c628ac00fa929cfb20594d805
SeraphDev6 <wesley.giles@gmail.com>
2021-08-12 02:06:44 -0400

    fixed it

--- server.py
+++ server.py
@@ -1,18 +1,22 @@
 from flask import Flask, render_template, request, redirect
-app = Flask(__name__)  
+import datetime
 
-@app.route('/')         
+app = Flask(__name__)
+
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
+@app.route('/checkout', methods=['POST'])
 def checkout():
-    print(request.form)
-    return render_template("checkout.html")
+    total = int(request.form['strawberry'])+int(request.form['raspberry'])+int(request.form['apple'])
+    now = datetime.datetime.now().strftime("%m/%d/%Y, at %H:%M %p")
+    print(f"Charging {request.form['first_name']} for {total} fruit")
+    return render_template("checkout.html",total= total,now = now)
 
-@app.route('/fruits')         
+@app.route('/fruits')
 def fruits():
     return render_template("fruits.html")
 
-if __name__=="__main__":   
-    app.run(debug=True)    
+if __name__=="__main__":
+    app.run(debug=True)
